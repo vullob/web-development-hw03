a479af85bfc01d5a9f0c7b55c4ddec146db57c4a
Brian Vullo <vullo.b@husky.neu.edu>
2019-01-23 20:26:16 -0500

    calc working

--- lib/practice/calc.ex
+++ lib/practice/calc.ex
@@ -11,7 +11,9 @@ defmodule Practice.Calc do
     |> String.split(~r/\s+/)
     |> tag_tokens
     |> convert_to_postfix
- #   |> IO.puts
+    |> IO.inspect
+    |> calculate
+ #  |> IO.puts
  #  |> hd
  #  |> parse_float
  #  |> :math.sqrt()
@@ -26,42 +28,83 @@ defmodule Practice.Calc do
   end
 
 
+  def calculate(expr) do
+    output = List.foldl(expr, [], fn x, acc -> evaluate_postfix(x, acc) end)
+    hd(output)
+  end
+
+
+  # acc is {output, [stack])
+  # element is the current element being read
+  def evaluate_postfix(element, acc) do
+    rhs = List.pop_at(acc, 0)
+    lhs = List.pop_at(elem(rhs, 1), 0)
+    case element do
+        {:num, x} -> [x|acc]
+        {:add, "+"} -> [elem(lhs, 0) + elem(rhs, 0) | elem(lhs, 1)]
+        {:sub, "-"} -> [elem(lhs, 0) - elem(rhs, 0) | elem(lhs, 1)]
+        {:mul, "*"} -> [elem(lhs, 0) * elem(rhs, 0) | elem(lhs, 1)]
+        {:div, "/"} -> [elem(lhs, 0) / elem(rhs, 0) | elem(lhs, 1)]
+    end
+  end
+
+
+  def div_postfix(acc) do
+    rhs = List.pop_at(acc, 0)
+    lhs = List.pop_at(elem(rhs,1), 0)
+    [elem(lhs, 0)/elem(rhs, 0) | elem(lhs, 1)]
+  end
+
+
+
+  def mul_postfix(acc) do
+    rhs = List.pop_at(acc, 0)
+    lhs = List.pop_at(elem(rhs,1), 0)
+    [elem(lhs, 0) * elem(rhs, 0) | elem(lhs, 1)]
+  end
+
+  def sub_postfix(acc) do
+    rhs = List.pop_at(acc, 0)
+    lhs = List.pop_at(elem(rhs,1), 0)
+    [elem(lhs, 0) - elem(rhs, 0) | elem(lhs, 1)]
+  end
+
+  def add_postfix(acc) do
+    rhs = List.pop_at(acc, 0)
+    lhs = List.pop_at(elem(rhs,1), 0)
+    [elem(lhs, 0) + elem(rhs, 0) | elem(lhs, 1)]
+  end
+
+
   def convert_to_postfix(arr) do
-    val = List.foldl(arr, {[], []}, fn x, acc -> val = postfix_char(x, acc);
-                                 IO.inspect(val); IO.inspect(val); val   end)
-     IO.puts("stored OPS")
-     IO.inspect(val)
-     elem(val, 0) ++ elem(val, 1) 
-     end
+    val = List.foldl(arr, {[], []}, fn x, acc -> postfix_char(x, acc) end)
+     elem(val, 0) ++ Enum.reverse(elem(val, 1))
+   end
 
  def postfix_char(char, accumulator) do
-    IO.puts("new char")
-    IO.inspect(char)
     acc = elem(accumulator, 0)
     storedOps = elem(accumulator, 1)
-    cond do 
-      length(storedOps) == 0 -> {acc, [char]}
-      true -> 
+    cond do
+      elem(char, 0) != :num && length(storedOps) == 0 -> {acc, [char]}
+      true ->
         case char do
-          {:num, x} -> { acc ++ [char], storedOps} 
-          {:add, x} -> { acc ++ Enum.reverse(storedOps), [char] } 
-          {:div, x} -> postfix_mul_div(char, acc, storedOps)
-          {:sub, x} -> { acc ++ Enum.reverse(storedOps), [char] }
-          {:mul, x} -> postfix_mul_div(char, acc, storedOps)
-        end   
+          {:num, x} -> { acc ++ [char], storedOps}
+          {:add, "+"} -> { acc ++ Enum.reverse(storedOps), [char] }
+          {:div, "/"} -> postfix_mul_div(char, acc, storedOps)
+          {:sub, "-"} -> { acc ++ Enum.reverse(storedOps), [char] }
+          {:mul, "*"} -> postfix_mul_div(char, acc, storedOps)
+        end
    end
 end
 
    def postfix_mul_div(char, acc, storedOps) do
     case hd(storedOps) do
-      {:add, x} -> { acc, storedOps ++ [char] }
-      {:sub, x} -> { acc, storedOps ++ [char] }
-      {:mul, x} -> { acc ++ Enum.reverse(storedOps), [char]}
-      {:div, x} -> { acc ++ Enum.reverse(storedOps), [char]}
+      {:add, "+"} -> { acc, storedOps ++ [char] }
+      {:sub, "-"} -> { acc, storedOps ++ [char] }
+      {:mul, "*"} -> { acc ++ Enum.reverse(storedOps), [char]}
+      {:div, "/"} -> { acc ++ Enum.reverse(storedOps), [char]}
     end
    end
-
-
 
   def tag_tokens(arr) do
     Enum.map(arr, fn x -> tokenize_char(x) end)
@@ -74,6 +117,6 @@ end
       char == "-" -> {:sub, char}
       char == "*" -> {:mul, char}
       Float.parse(char) -> {:num, elem(Float.parse(char), 0)}
-    end 
+    end
   end
 end
